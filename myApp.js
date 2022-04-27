aa1585c1f3723a2ef209824b0be789d10a3e9c5b
Lamadar <51517744+Lamadar@users.noreply.github.com>
2022-04-27 18:09:51 +0000

    add request, path and ip logging

--- myApp.js
+++ myApp.js
@@ -2,15 +2,23 @@ var express = require('express');
 var app = express();
 
 console.log('Hello World')
+
 app.use("/public", express.static(__dirname + "/public"));
+
+app.use(function(req, res, next) {
+      console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get('/json', function(req, res) {
-    res.send({"message": "Hello json"});
+    res.send({"message": (process.env.MESSAGE_STYLE == "uppercase") ? "Hello json".toUpperCase() : "Hello json" });
 });
+
+
 
 
 
